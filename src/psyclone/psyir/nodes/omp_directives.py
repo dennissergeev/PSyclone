51902f172b092a5e635788f4b1e857e9c4863f28
Aidan Chalk <3043914+LonelyCat124@users.noreply.github.com>
2021-12-21 10:04:22 +0000

    #1338 Fix simple lint issues

--- src/psyclone/psyir/nodes/omp_directives.py
+++ src/psyclone/psyir/nodes/omp_directives.py
@@ -685,7 +685,7 @@ class OMPTaskDirective(OMPRegionDirective):
                     elif type(index) is BinaryOperation:
                         # Binary Operation check
                         OMPTaskDirective.handle_binary_op(index,
-                                index_strings, firstprivate_list,
+                                index_list, firstprivate_list,
                                 private_list, parallel_private,
                                 start_val, stop_val)
                     else:
@@ -782,6 +782,7 @@ class OMPTaskDirective(OMPRegionDirective):
         :raises GenerationError: If the child Reference is a shared variable.
         '''
         from psyclone.psyir.backend.fortran import FortranWriter
+        writer = FortranWriter()
         # Binary Operation check
         if node.operator is not \
            BinaryOperation.ADD and \
@@ -823,7 +824,7 @@ class OMPTaskDirective(OMPRegionDirective):
                 # FIXME I think this should be
                 # supportable, but need to think
                 if node.operator is \
-                    BinaryOperator.ADD:
+                    BinaryOperation.ADD:
                     index_name = index_name + ("+({0} - {1})"
                            .format(writer(stop_val)
                                ,writer(start_val)))
@@ -835,7 +836,7 @@ class OMPTaskDirective(OMPRegionDirective):
             elif index_name in firstprivate_list:
                 # name +/- stop-start
                 if node.operator is \
-                    BinaryOperator.ADD:
+                    BinaryOperation.ADD:
                     index_name = index_name + ("+({0} - {1})"
                            .format(writer(stop_val)
                                ,writer(start_val)))
@@ -847,8 +848,8 @@ class OMPTaskDirective(OMPRegionDirective):
             else:
                 firstprivate_list.append(index_name)
                 # name +/- stop-start 
-                if index.operator is \
-                    BinaryOperator.ADD:
+                if node.operator is \
+                    BinaryOperation.ADD:
                     index_name = index_name + ("+({0} - {1})"
                            .format(writer(stop_val)
                                ,writer(start_val)))
@@ -1018,7 +1019,7 @@ class OMPTaskDirective(OMPRegionDirective):
                         elif isinstance(index, Reference):
                             # Refrences also just treated as values
                             index_strings.append(writer(index))
-                            if index.symbol == loop_val.symbol:
+                            if index.symbol == loop_var.symbol:
                                 loop_reference = True
                         elif isinstance(index, BinaryOperation):
                             # Binary Operation check
